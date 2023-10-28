b8049afae09e2e749e5157f6879d8abeb65d3876
Microchesst <Marcusnj123@gmail.com>
2023-10-28 11:04:25 +0200

    Ændringer så ingen fejl indtil videre!

--- DAir/Models/Rating.cs
+++ DAir/Models/Rating.cs
@@ -6,6 +6,16 @@
         public int EmployeeID { get; set; }
         public int RatingValue { get; set; }
 
+        // For the rater (employee who is rating)
+        public int RaterID { get; set; } // Foreign key property 
+        public Employee Rater { get; set; }
+
+        // For the ratee (pilot who is being rated)
+        public int RateeID { get; set; } // Foreign key property (type should match Pilot's primary key)
+        public Pilot Ratee { get; set; } // Navigation property
+
+
+
         // Navigation properties
         public Pilot Pilot { get; set; }
         public Employee Employee { get; set; }

6926eaadddcd7132178542015a9712e1170fcfc5
Microchesst <Marcusnj123@gmail.com>
2023-10-28 21:19:33 +0200

    Dont know where i am am!

--- DAir/Context/DbInitializer.cs
+++ DAir/Context/DbInitializer.cs
@@ -1,4 +1,7 @@
 ﻿using DAir.Models;
+using System; // For DateTime
+using System.Collections.Generic; // For List support
+using System.Linq; // For checking if the context already contains data
 
 namespace DAir.Context
 {
@@ -8,61 +11,106 @@ namespace DAir.Context
         {
             context.Database.EnsureCreated();
 
+            // Clear existing data (Reseeding)
+            context.Languages.RemoveRange(context.Languages);
+            context.CabinMembers.RemoveRange(context.CabinMembers);
+            context.Pilots.RemoveRange(context.Pilots);
+            context.Employees.RemoveRange(context.Employees);
+            context.Flights.RemoveRange(context.Flights);
+            context.FlightSchedules.RemoveRange(context.FlightSchedules);
+            context.Ratings.RemoveRange(context.Ratings);
+
             // Check if the database has been seeded already
             if (context.Employees.Any())
             {
                 return;   // DB has been seeded
             }
 
+            // Ensure all changes are committed before seeding new data
+            context.SaveChanges();
+
             // Employees
             var employees = new List<Employee>
-        {
-            new Employee { FirstName = "Noah", LastName = "Smith", Role = "Pilot" },
-            new Employee { FirstName = "Laerke", LastName = "Jensen", Role = "Cabin Crew" },
-            new Employee { FirstName = "Anton", LastName = "Nielsen", Role = "Cabin Crew" }
-            // Add more employees as needed
-        };
+            {
+                new Employee { FirstName = "Noah", LastName = "Smith", Role = "Pilot" },
+                new Employee { FirstName = "Laerke", LastName = "Jensen", Role = "Cabin Crew" },
+                new Employee { FirstName = "Anton", LastName = "Nielsen", Role = "Cabin Crew" },
+                // Add more employees...
+                new Employee { FirstName = "Eva", LastName = "Green", Role = "Pilot" },
+                new Employee { FirstName = "Liam", LastName = "Brown", Role = "Cabin Crew" },
+                // Adding more for diverse data
+                new Employee { FirstName = "Olivia", LastName = "Williams", Role = "Pilot" },
+                new Employee { FirstName = "Emma", LastName = "Johnson", Role = "Cabin Crew" },
+                // ... more employees as needed
+            };
             context.Employees.AddRange(employees);
 
             // Pilots
             var pilots = new List<Pilot>
-        {
-            new Pilot { Employee = employees[0], GeoLocation = "CPH", Certification = "Airbus 350", Rank = "Captain" },
-            // Add more pilots as needed
-        };
+            {
+                new Pilot { Employee = employees[0], GeoLocation = "CPH", Certification = "Airbus 350", Rank = "Captain" },
+                new Pilot { Employee = employees[3], GeoLocation = "SFO", Certification = "Boeing 777", Rank = "First Officer" },
+                new Pilot { Employee = employees[5], GeoLocation = "CPH", Certification = "Airbus 320", Rank = "Captain" },
+                // ... more pilots as needed
+            };
             context.Pilots.AddRange(pilots);
 
             // Cabin Members
             var cabinMembers = new List<CabinMember>
-        {
-            new CabinMember { Employee = employees[1], GeoLocation = "CPH", Certification = "Safety", Languages = new List<Languages> { new Languages { Language = "Danish" }, new Languages { Language = "English" } } },
-            new CabinMember { Employee = employees[2], GeoLocation = "CPH", Certification = "Service", Languages = new List<Languages> { new Languages { Language = "Danish" }, new Languages { Language = "French" } } }
-            // Add more cabin members as needed
-        };
+            {
+                new CabinMember { Employee = employees[1], GeoLocation = "CPH", Certification = "Safety", Languages = new List<Languages> { new Languages { Language = "Danish" }, new Languages { Language = "English" } } },
+                new CabinMember { Employee = employees[2], GeoLocation = "CPH", Certification = "Service", Languages = new List<Languages> { new Languages { Language = "Danish" }, new Languages { Language = "French" } } },
+                new CabinMember { Employee = employees[4], GeoLocation = "SFO", Certification = "Safety", Languages = new List<Languages> { new Languages { Language = "English" }, new Languages { Language = "Spanish" } } },
+                new CabinMember { Employee = employees[6], GeoLocation = "SFO", Certification = "Service", Languages = new List<Languages> { new Languages { Language = "English" }, new Languages { Language = "German" } } },
+                // ... more cabin members as needed
+            };
             context.CabinMembers.AddRange(cabinMembers);
+            context.SaveChanges();
+
+            // Languages
+            var languages = new List<Languages>
+            {
+                new Languages { Language = "Danish", CabinMemberID = cabinMembers[0].CabinMemberID },
+                new Languages { Language = "English", CabinMemberID = cabinMembers[0].CabinMemberID },
+                new Languages { Language = "Danish", CabinMemberID = cabinMembers[1].CabinMemberID },
+                new Languages { Language = "French", CabinMemberID = cabinMembers[1].CabinMemberID },
+                // ... additional languages for other cabin members
+            };
+
+            context.Languages.AddRange(languages);
 
             // Flights
             var flights = new List<Flight>
-        {
-            new Flight { FlightCode = "SK935", DepartureAirport = "CPH", ArrivalAirport = "SFO", ScheduledDepartureTime = new DateTime(2023, 10, 28, 16, 15, 0), ScheduledArrivalTime = new DateTime(2023, 10, 28, 18, 35, 0), State = "Scheduled", AircraftID = 1 },
-            // Add more flights as needed
-        };
+            {
+                new Flight { FlightCode = "SK935", DepartureAirport = "CPH", ArrivalAirport = "SFO", ScheduledDepartureTime = new DateTime(2023, 10, 10, 16, 15, 0), ScheduledArrivalTime = new DateTime(2023, 10, 10, 18, 35, 0), State = "Scheduled" },
+                new Flight { FlightCode = "LH123", DepartureAirport = "SFO", ArrivalAirport = "CPH", ScheduledDepartureTime = new DateTime(2023, 10, 11, 10, 0, 0), ScheduledArrivalTime = new DateTime(2023, 10, 11, 14, 30, 0), State = "Canceled" },
+                new Flight { FlightCode = "BA456", DepartureAirport = "CPH", ArrivalAirport = "LHR", ScheduledDepartureTime = new DateTime(2023, 10, 12, 9, 0, 0), ScheduledArrivalTime = new DateTime(2023, 10, 12, 11, 20, 0), State = "Delayed" },
+                // ... more flights as needed
+            };
             context.Flights.AddRange(flights);
 
             // Flight Schedules
             var flightSchedules = new List<FlightSchedule>
-        {
-            new FlightSchedule { Flight = flights[0], Employee = employees[0] },
-            // Add more flight schedules as needed
-        };
+            {
+                new FlightSchedule { Flight = flights[0], Employee = employees[0] },
+                new FlightSchedule { Flight = flights[0], Employee = employees[1] },
+                new FlightSchedule { Flight = flights[1], Employee = employees[3] },
+                new FlightSchedule { Flight = flights[2], Employee = employees[5] },
+                new FlightSchedule { Flight = flights[2], Employee = employees[6] },
+                // ... more flight schedules as needed
+            };
             context.FlightSchedules.AddRange(flightSchedules);
 
             // Ratings
             var ratings = new List<Rating>
-        {
-            new Rating { Rater = employees[0], Ratee = pilots[0], RatingValue = 4 },
-            // Add more ratings as needed
-        };
+            {
+                new Rating { Rater = employees[0], Ratee = pilots[1], RatingValue = 4 },
+                new Rating { Rater = employees[3], Ratee = pilots[0], RatingValue = 5 },
+                // ... more ratings as needed
+                new Rating { Rater = employees[5], Ratee = pilots[2], RatingValue = 3 },
+                new Rating { Rater = employees[6], Ratee = pilots[1], RatingValue = 4 },
+                // ... more ratings for robust data
+            };
             context.Ratings.AddRange(ratings);
 
             context.SaveChanges();
--- DAir/Controllers/QueriesController.cs
+++ DAir/Controllers/QueriesController.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 
 namespace DAir.Controllers
 {
+
+    // Additional class definition for EmployeeFlightSchedule
+    public class EmployeeFlightSchedule
+    {
+        public string EmployeeName { get; set; }
+        public string Airport { get; set; }
+        public int FlightCount { get; set; }
+    }
+
     [Route("api/[controller]")]
     [ApiController]
     public class QueriesController : ControllerBase
@@ -34,7 +43,117 @@ namespace DAir.Controllers
 
             return flight;
         }
+        // 2. Crew Members Available for Specific Aircraft at an Airport
+        [HttpGet("GetCrewMembersForAircraftAtAirport/{aircraftType}/{airport}")]
+        public async Task<ActionResult<List<string>>> GetCrewMembersForAircraftAtAirport(string aircraftType, string airport)
+        {
+            var pilots = await _context.Pilots
+                .Where(p => p.Certification == aircraftType && p.GeoLocation == airport)
+                .Select(p => p.Employee.FirstName + " " + p.Employee.LastName)
+                .ToListAsync();
 
-        // Other query methods...
+            var cabinMembers = await _context.CabinMembers
+                .Where(cm => cm.Certification == aircraftType && cm.GeoLocation == airport)
+                .Select(cm => cm.Employee.FirstName + " " + cm.Employee.LastName)
+                .ToListAsync();
+
+            var crewMembers = pilots.Concat(cabinMembers).Distinct().ToList();
+
+            return crewMembers;
+        }
+
+        // 3. Count of Canceled Flights
+        [HttpGet("GetCanceledFlightsCount")]
+        public async Task<ActionResult<int>> GetCanceledFlightsCount()
+        {
+            int canceledFlightsCount = await _context.Flights
+                .CountAsync(f => f.State == "Canceled");
+
+            return canceledFlightsCount;
+        }
+
+        // 4. Flight Schedule per Employee by Airport - Simplified
+        [HttpGet("GetFlightScheduleByEmployee")]
+        public async Task<ActionResult<IEnumerable<EmployeeFlightSchedule>>> GetFlightScheduleByEmployee()
+        {
+            var flightSchedules = await _context.FlightSchedules
+                .Include(fs => fs.Employee)
+                .Include(fs => fs.Flight)
+                .GroupBy(fs => new { fs.Employee.FirstName, fs.Employee.LastName, fs.Flight.DepartureAirport })
+                .Select(group => new EmployeeFlightSchedule
+                {
+                    EmployeeName = group.Key.FirstName + " " + group.Key.LastName,
+                    Airport = group.Key.DepartureAirport,
+                    FlightCount = group.Count()
+                })
+                .OrderBy(efs => efs.EmployeeName)
+                .ThenBy(efs => efs.Airport)
+                .ToListAsync();
+
+            return flightSchedules;
+        }
+
+
+
+
+        // 5. Average Rating Given by a Pilot
+        [HttpGet("GetAverageRatingByPilot/{pilotName}")]
+        public async Task<ActionResult<double>> GetAverageRatingByPilot(string pilotName)
+        {
+            var pilot = await _context.Pilots
+                .Include(p => p.Employee)
+                .Where(p => p.Employee.FirstName + " " + p.Employee.LastName == pilotName)
+                .FirstOrDefaultAsync();
+
+            if (pilot == null)
+            {
+                return NotFound("Pilot not found.");
+            }
+
+            var averageRating = await _context.Ratings
+                .Where(r => r.RateeID == pilot.PilotID)
+                .DefaultIfEmpty()
+                .AverageAsync(r => r == null ? 0 : r.RatingValue);
+
+            return averageRating;
+        }
+
+        // 6. Languages Spoken by a Cabin Crew Member
+        [HttpGet("GetLanguagesByCrewMember/{crewMemberName}")]
+        public async Task<ActionResult<List<string>>> GetLanguagesByCrewMember(string crewMemberName)
+        {
+            var crewMemberLanguages = await _context.CabinMembers
+                .Include(cm => cm.Employee)
+                .Include(cm => cm.Languages)
+                .Where(cm => cm.Employee.FirstName + " " + cm.Employee.LastName == crewMemberName)
+                .SelectMany(cm => cm.Languages)
+                .Select(l => l.Language) // Adjusted to the correct property name of Languages
+                .ToListAsync();
+
+            return crewMemberLanguages;
+        }
+        // 7. Pilot with Highest Average Rating
+        [HttpGet("GetPilotWithHighestAverageRating")]
+        public async Task<ActionResult<string>> GetPilotWithHighestAverageRating()
+        {
+            var highestRatedPilot = await _context.Ratings
+                .GroupBy(r => r.RateeID)
+                .Select(group => new
+                {
+                    PilotID = group.Key,
+                    AverageRating = group.Average(r => r.RatingValue)
+                })
+                .OrderByDescending(r => r.AverageRating)
+                .Select(r => r.PilotID)
+                .FirstOrDefaultAsync();
+
+            var pilotName = await _context.Pilots
+                .Where(p => p.PilotID == highestRatedPilot)
+                .Select(p => p.Employee.FirstName + " " + p.Employee.LastName)
+                .FirstOrDefaultAsync();
+
+            return pilotName;
+        }
+
     }
 }
